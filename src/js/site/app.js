bee72ded133f406c46a59667b5da13503bb2656b
hdavtian <harmadavtian@gmail.com>
2018-01-02 18:26:24 -0800

    added header directive ...

--- src/js/site/app.js
+++ src/js/site/app.js
@@ -4,6 +4,7 @@ import appConfig                from './app-config';
 import appCtrl                  from './app-ctrl';
 import dataService              from './services/hd-data-service';
 import hdNavbar                 from './hd-navbar/hd-navbar-directive';
+import hdHeader                 from './hd-header/hd-header-directive';
 
 // app --------------------------------------------------------------
 
@@ -14,3 +15,4 @@ app
     .controller('appCtrl', ['$scope', appCtrl])
     .service('dataService', dataService)
     .directive('hdNavbar', hdNavbar)
+    .directive('hdHeader', hdHeader);
---
+++ src/js/site/hd-header/hd-header-directive.js
@@ -0,0 +1,13 @@
+export default function(){
+    return {
+        templateUrl: 'hd-header/hd-header.html',
+        controllerAs: 'hdHeader',
+        replace: true,
+        controller: ['$scope', function($scope){
+            //
+        }],
+        link: function($scope, $element, $attrs) {
+
+        }
+    }
+}
