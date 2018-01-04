a29ce32c6cb036c70d2c33b6074dc4c9052f5c9f
hdavtian <harmadavtian@gmail.com>
2018-01-03 18:03:08 -0800

    randomness is cool!

--- src/js/site/pages/about/about-ctrl.js
+++ src/js/site/pages/about/about-ctrl.js
@@ -1,20 +1,34 @@
 import Parallax from 'parallax-js';
 
 export default function($scope, $timeout){
+
     $scope.setBodyBackgroundColor('#2b2b2b');
+
 
     var scene = document.getElementById('scene');
     var layers = ['layer1', 'layer2', 'layer3', 'layer4', 'layer5'];
     var depth = 0;
-    // setting `data-depth` attribute for each div
-    for (var i=0, l=layers.length; i<l; i++){
-        $('.' + layers[i]).attr('data-depth', depth += 0.2)
-    }
+    var $el;
+    var randScale;
+
+    var randomBetween = function(a, b){
+        return Math.floor(Math.random() * b) + a;
+    };
+
+    $('.layer').each(function(){
+        $(this).attr('data-depth', depth += randomBetween(1,5)/10);
+        randScale = randomBetween(10, 30)/11;
+        $(this).find('.i').css({
+            'top' : randomBetween(1, scene.offsetHeight - 250) + 'px',
+            'left' : randomBetween(1, scene.offsetWidth - 250) + 'px',
+            'transform' : 'scale(' + randScale + ',' + randScale +')'
+        });
+    })
 
     var parallaxInstance = new Parallax(scene, {
         // relativeInput: true,
         // clipRelativeInput: true,
-        hoverOnly: true,
+        // hoverOnly: true,
         pointerEvents: true
     });
 
