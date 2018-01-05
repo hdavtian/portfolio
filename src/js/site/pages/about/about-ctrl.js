261f15fb3659b8b40f1946b0d2d6d59351fe5cc2
hdavtian <harmadavtian@gmail.com>
2018-01-04 21:00:17 -0800

    about page ...

--- src/js/site/pages/about/about-ctrl.js
+++ src/js/site/pages/about/about-ctrl.js
@@ -24,8 +24,8 @@ export default function($scope, $timeout){
         randScale = randomBetween(8, 20)/11;
         $(this).find('.i').css({
             'top' : randomBetween(1, scene.offsetHeight - 250) + 'px',
-            'left' : randomBetween(1, scene.offsetWidth - 250) + 'px',
-            'transform' : 'scale(' + randScale + ',' + randScale +')'
+            'left' : randomBetween(1, scene.offsetWidth - 250) + 'px'//,
+            //'transform' : 'scale(' + randScale + ',' + randScale +')'
         });
         $(this).find('a').css({
             'background-color' : 'rgba('+randomBetween(0,255)+','+randomBetween(0,255)+','+randomBetween(0,255)+','+0.75+')'
@@ -39,32 +39,56 @@ export default function($scope, $timeout){
         pointerEvents: true
     });
 
-    // testing TimelineMax and ScrollMagic
+    $scope.thingActions = (function(){
 
-
-    /*
-
-    var controller = new ScrollMagic.Controller();
-    var topnavAnimation = new TimelineMax({paused:true})
-        .to('.about-page-wrapper', 15, {height:'110px', ease: Power4.easeOut}, 'start')
-        .add('end');
-    var topnavScene = new ScrollMagic.Scene({
-        offset: 0//,
-        //duration: 300
-    });
-    topnavScene.setTween(topnavAnimation.play());
-//topnavScene.addIndicators();
-    controller.addScene([topnavScene]);
-    */
-
-
-    var resetThings = function(){
         var $i = $('.i');
-        new TimelineMax()
-            .staggerTo($i, 2, {left:'110px', ease: Power4.easeOut}, 0.25, 'start')
-    };
+        var originalPositions = [];
 
-    resetThings();
+        var contract = function(){
+            new TimelineMax()
+                .staggerTo($i, 1, {left:'110px', ease: Power4.easeOut}, 0.15, 'start')
+        };
+
+        var expand = function(){
+            console.log('expanding...');
+            for(var i=0, l=originalPositions.length; i<l; i++){
+                new TimelineMax()
+                    .to(originalPositions[i].$el, 1, {
+                        left:originalPositions[i].x,
+                        top:originalPositions[i].y,
+                        ease: Power4.easeOut
+                    }, 'start');
+            }
+            /*
+            $i.each(function($index){
+                new TimelineMax()
+                    .to($i, 1, {
+                        left:originalPositions[$index].x,
+                        top:originalPositions[$index].y,
+                        ease: Power4.easeOut
+                    }, 'start');
+                console.log(originalPositions[$index].x + ',' +originalPositions[$index].y)
+            });
+            */
+        };
+
+        var _constructor = function(){
+            $i.each(function(){
+                originalPositions.push({$el: $(this), x: $(this).css('left'), y: $(this).css('top')})
+            });
+            console.log(originalPositions);
+        };
+
+        _constructor();
+
+        return {
+            contract: contract,
+            expand: expand
+        }
+
+    })();
+
+    //resetThings();
 
 
 
