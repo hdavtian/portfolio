db70d32c0d45461aef30d6e35100ee02a6f7bdd0
hdavtian <harmadavtian@gmail.com>
2018-01-07 17:20:19 -0800

    adding content

--- src/js/site/pages/portfolio/portfolio-ctrl.js
+++ src/js/site/pages/portfolio/portfolio-ctrl.js
@@ -19,13 +19,15 @@ export default function($scope, $timeout, dataService){
     var moduleMouseoverAnimation = function(_el) {
 
         var $el = _el.find('.bgImg');
-        var $title = _el.find('.wrapper h2');
-        var $line = _el.find('.title p');
+        var $info = _el.find('.info');
+        // var $title = _el.find('.wrapper h2');
+        // var $line = _el.find('.title p');
 
         var animation = new TimelineMax({ paused: true })
             .add('part1')
             .to($el, 1.5, { transform: 'scale(1.5)', ease: Power4.easeOut, y: 0 }, 'part1')
-            .to($line, 1.5, {width: '100%', ease: Power4.easeOut}, 'part1')
+            .to($info, 0.75, {bottom: 0, ease: Power4.easeOut}, 'part1')
+            // .to($line, 1.5, {width: '100%', ease: Power4.easeOut}, 'part1')
             .add('end');
 
         animation.play();
@@ -34,13 +36,15 @@ export default function($scope, $timeout, dataService){
     var moduleMouseoutAnimation = function (_el) {
 
         var $el = _el.find('.bgImg');
-        var $title = _el.find('.wrapper h2');
-        var $line = _el.find('.title p');
+        var $info = _el.find('.info');
+        // var $title = _el.find('.wrapper h2');
+        // var $line = _el.find('.title p');
 
         var animation = new TimelineMax({ paused: true })
             .add('part1')
             .to($el, 1.5, { transform: 'scale(1.0)', ease: Power4.easeOut, y: 0, clearProps: "transform" }, 'part1')
-            .to($line, 1.5, { width: 'auto', ease: Power4.easeOut }, 'part1')
+            // .to($line, 1.5, { width: 'auto', ease: Power4.easeOut }, 'part1')
+            .to($info, 0.75, {bottom: '-90px', ease: Power4.easeOut}, 'part1')
             .add('end');
 
         animation.play();
